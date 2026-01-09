9193ca0bd954a1c1127018e4802701ac00f5d960
marrisonlab <43366725+marrisonlab@users.noreply.github.com>
2026-01-09 09:15:23 +0100

    Update custom_updater.php

--- custom_updater.php
+++ custom_updater.php
@@ -3,7 +3,7 @@
  * Plugin Name: Marrison Custom Updater
  * Plugin URI:  https://marrisonlab.com
  * Description: Updater custom con repository remoto, update reale dei file, singolo e bulk.
- * Version: 1.6.1
+ * Version: 1.6.3
  * Author: Angelo Marra
  * Author URI:  https://marrisonlab.com
  */
